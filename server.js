c6f412240688db4ba6c496ad2175ea608344c62e
Santi Acosta Verrier <santiav@gmail.com>
2026-09-11 14:31:15 -0300

    config inicial con ej de /productos

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors'
+const app = express();
+const port = 3000;
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Servidor disponible 🟢');
+});
+
+app.get('/productos', (req, res) => {
+    res.json([
+        {
+            id: 1,
+            nombre: "Producto #1",
+            precio: 1000
+        },
+        {
+            id: 2,
+            nombre: "Producto #2",
+            precio: 2000
+        }
+    ])
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
