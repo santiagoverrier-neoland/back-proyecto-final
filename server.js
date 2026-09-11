ae232e56e301ebe4fbaaaed5cc8236e609807ace
Santi Acosta Verrier <santiav@gmail.com>
2026-09-11 14:47:10 -0300

    preparación hosting render

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 
@@ -25,5 +25,5 @@ app.get('/productos', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Backend disponible en puerto: ${port}`);
 });
